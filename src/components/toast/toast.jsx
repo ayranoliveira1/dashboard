b1286e4d595eb07693b49213cb920f40564ee32f
ayranoliveira1 <ayranoliveira1@gmail.com>
2024-03-26 14:13:19 -0300

    Feat: add Toast comoponent

---
+++ src/components/toast/toast.jsx
@@ -0,0 +1,16 @@
+import { ReactNode } from "react";
+import { ToastContainer } from "react-toastify";
+
+import "react-toastify/dist/ReactToastify.css";
+
+const ToastProvider = ({ children }) => {
+   return (
+      <>
+         {children}
+
+         <ToastContainer />
+      </>
+   );
+};
+
+export default ToastProvider;
